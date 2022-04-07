ececfd7d09c807687e7114059130caadb44217ed
Mukles <Mukleshossen1763@gmail.com>
2022-04-07 21:52:58 +0600

    review question readme file edited done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Dashbaord from "./Components/Dashboard";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import NotFound from "./Components/NotFound";
+import QuestionAnswer from "./Components/QuestionAnswer";
 import Reviews from "./Components/Reviews";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/review" element={<Reviews />} />
         <Route exact path="/dashboard" element={<Dashbaord />} />
+        <Route exact path="/question" element={<QuestionAnswer />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </React.Fragment>
--- src/Components/QuestionAnswer.jsx
+++ src/Components/QuestionAnswer.jsx
@@ -1,10 +1,35 @@
 const QuestionAnswer = () => {
   return (
-    <div className="">
+    <div className="container mx-auto px-5 pt-10">
       <div className="mb-4">
-        <strong>
+        <strong className="text-xl">What is context api?</strong>
+        <p className="text-lg font-medium">
+          The Context API is a React structure that enables you to exchange
+          unique details and assists in solving prop-drilling from all levels of
+          your application.
+        </p>
+      </div>
+      <div className="mb-4">
+        <strong className="text-xl">What is semantics in html?</strong>
+        <p className="text-lg font-medium">
+          The HTML semantics refers to the tags that provide meaning to an HTML
+          page rather than just presentation. It makes HTML more comprehensible
+          by better defining the different sections and layout of web pages.
+        </p>
+      </div>
+      <div className="mb-4">
+        <strong className="text-xl">
           What is the differnt between "Inline tag and block tag" ?
         </strong>
+        <p className="text-lg font-medium">
+          Compared to display: inline, the major difference is that display:
+          inline-block allows to set a width and height on the element. Also,
+          with display: inline-block, the top and bottom margins/paddings are
+          respected, but with display: inline they are not. Compared to display:
+          block, the major difference is that display: inline-block does not add
+          a line-break after the element, so the element can sit next to other
+          elements.
+        </p>
       </div>
 
       <div className="">
