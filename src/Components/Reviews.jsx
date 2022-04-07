3e66a01158ba0ac923ecabd17c8518e7ab15fd12
Mukles <Mukleshossen1763@gmail.com>
2022-04-07 22:12:30 +0600

    review list are edited on view port

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,15 +1,20 @@
+import { useLocation } from "react-router-dom";
 import { useFetchData } from "../hook/fetchingData";
 import ReviewList from "./ReviewList";
 
 const Reviews = () => {
   const [data] = useFetchData();
+  const { pathname } = useLocation();
+  console.log(data.slice(3));
 
   return (
     <div className="reviews w-1/2 mx-auto  flex flex-col justify-center items-center space-y-3">
       <h1 className="text-xl font-bold text-center">What our customer says?</h1>
-      {data.map((Reviews) => (
-        <ReviewList key={Reviews.id} {...Reviews} />
-      ))}
+      {pathname === "/"
+        ? data
+            .slice(3)
+            .map((Reviews) => <ReviewList key={Reviews.id} {...Reviews} />)
+        : data.map((Reviews) => <ReviewList key={Reviews.id} {...Reviews} />)}
     </div>
   );
 };
